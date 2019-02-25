736120e43bde460af444b4de2b9ddf9d4e9e0f7f
Shaw Young <Shawson@gmail.com>
2019-02-25 18:43:14 +0000

    updating the launcher scripts to output to autoconfig

--- EmulationLaunchers/retroarch/retroarch.ps1
+++ EmulationLaunchers/retroarch/retroarch.ps1
@@ -23,16 +23,17 @@ Catch {
 }
 
 log("successfully found controller ids- generating config")
-<#
+
 $(
+    # not everything should be xinput- check flag in windows device name
     Write-Output "input_driver = ""xinput"""
-    Write-Output "input_device = ""$($gpdOutput.BaseCommandLineResponseOfGameControllerIdentifiers.Data.DeviceName)"""
+    Write-Output "input_device = ""$(GetControllerName $gpdOutput.BaseCommandLineResponseOfGameControllerIdentifiers.Data.DeviceName)"""
     Write-Output "input_vendor_id = ""$($gpdOutput.BaseCommandLineResponseOfGameControllerIdentifiers.Data.VID)"""
     Write-Output "input_product_id = ""$($gpdOutput.BaseCommandLineResponseOfGameControllerIdentifiers.Data.PID)"""
 
     $lastInput.input | ForEach-Object { GetMappedControl -type $_.type -name $_.name -id $_.id -value $_.value }
 ) | Out-File "$retroWinRoot\emulators\retroarch\retroarch.cfg"
-#>
+
 
 log("config file written to $retroWinRoot\emulators\retroarch\retroarch.cfg")
 
